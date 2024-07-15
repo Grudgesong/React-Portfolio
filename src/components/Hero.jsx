915ebea94cf0d3c6b270286a8fb652dbaf3ebf1d
Sangraj Ranjit <Grudgesong@gmail.com>
2024-07-15 22:57:33 +0800

    fixed use of ' in hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,9 @@ export default function Hero() {
     <div className="bg-emerald-100 py-24">
       <div className="align-element grid md:grid-cols-2 items-center gap-8">
         <article>
-          <h1 className="text-7xl font-bold tracking-wider">I'm Sangraj</h1>
+          <h1 className="text-7xl font-bold tracking-wider">
+            I&apos;m Sangraj
+          </h1>
           <p className="mt-4 text-3xl text-slate-700 capitalize tracking-wide">
             <Typewriter
               words={["Front-End", "Full Stack"]}
